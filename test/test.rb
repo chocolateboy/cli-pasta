29e27eb441261f1734810cbb8243e0d59422aa2f
chocolateboy <chocolate@cpan.org>
2018-03-28 02:16:57 +0100

    travis test

--- test/test.rb
+++ test/test.rb
@@ -4,45 +4,47 @@ require 'komenda'
 require 'test/unit'
 require 'tty-which'
 
-# XXX `ruby -Ilib` is needed for the tests to work work on Travis.
-# otherwise, attempts to load clipasta/epipe and clipasta/sigint from nested ruby
-# calls die with a LoadError.
+# a wrapper for `Komenda.run` which ensures the bundler environment is propagated to
+# nested ruby calls.
+def run_command(command) # XXX run is already taken by Test::Unit::TestCase
+  Komenda.run(command, use_bundler_env: true)
+end
 
 class CLIPastaTest < Test::Unit::TestCase
   TIMEOUT = TTY::Which.which('timeout') || TTY::Which.which('gtimeout')
 
   def test_epipe
-    result = Komenda.run %q[ruby -e 'loop { puts "." }' | head -n0]
+    result = run_command %q[ruby -e 'loop { puts "." }' | head -n0]
     assert { result.stderr =~ /EPIPE/ }
-    result = Komenda.run %q[ruby -Ilib -r clipasta -e 'loop { puts "." }' | head -n0]
+    result = run_command %q[ruby -r clipasta -e 'loop { puts "." }' | head -n0]
     assert { result.stderr !~ /EPIPE/ }
   end
 
   def test_sigint
     if TIMEOUT
-      result = Komenda.run %[#{TIMEOUT} --signal INT 1 ruby -e sleep]
+      result = run_command %[#{TIMEOUT} --signal INT 1 ruby -e sleep]
       assert { result.stderr =~ /Interrupt/ }
-      result = Komenda.run %[#{TIMEOUT} --signal INT 1 ruby -Ilib -r clipasta -e sleep]
+      result = run_command %[#{TIMEOUT} --signal INT 1 ruby -r clipasta -e sleep]
       assert { result.stderr !~ /Interrupt/ }
     end
   end
 
   def test_epipe_no_sigint
-    result = Komenda.run %q[ruby -Ilib -r clipasta/epipe -e 'loop { puts "." }' | head -n0]
+    result = run_command %q[ruby -r clipasta/epipe -e 'loop { puts "." }' | head -n0]
     assert { result.stderr !~ /EPIPE/ }
 
     if TIMEOUT
-      result = Komenda.run %[#{TIMEOUT} --signal INT 1 ruby -Ilib -r clipasta/epipe -e sleep]
+      result = run_command %[#{TIMEOUT} --signal INT 1 ruby -r clipasta/epipe -e sleep]
       assert { result.stderr =~ /Interrupt/ }
     end
   end
 
   def test_sigint_no_epipe
-    result = Komenda.run %q[ruby -Ilib -r clipasta/sigint -e 'loop { puts "." }' | head -n0]
+    result = run_command %q[ruby -r clipasta/sigint -e 'loop { puts "." }' | head -n0]
     assert { result.stderr =~ /EPIPE/ }
 
     if TIMEOUT
-      result = Komenda.run %[#{TIMEOUT} --signal INT 1 ruby -Ilib -r clipasta/sigint -e sleep]
+      result = run_command %[#{TIMEOUT} --signal INT 1 ruby -r clipasta/sigint -e sleep]
       assert { result.stderr !~ /Interrupt/ }
     end
   end
