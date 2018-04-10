d950f0851fc68ba58511c0dd501401a847e7b43f
chocolateboy <chocolate@cpan.org>
2018-04-10 19:52:32 +0100

    log skipped tests if timeout(1) is not available

--- test/test.rb
+++ test/test.rb
@@ -5,9 +5,8 @@ require 'minitest/autorun'
 require 'minitest-power_assert'
 require 'tty-which'
 
-# ensure this gem is visible in nested ruby calls
-RUBY = 'ruby -r bundler/setup'
-
+RUBY = 'ruby -r bundler/setup' # ensure this gem is visible in nested ruby calls
+NO_TIMEOUT = 'timeout(1) is not available'
 TIMEOUT = TTY::Which.which('timeout') || TTY::Which.which('gtimeout')
 
 # XXX can't use `run` as that's a minitest builtin
@@ -17,12 +16,11 @@ end
 
 describe 'cli-pasta' do
   it 'handles SIGINT' do
-    if TIMEOUT
-      result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -e sleep]
-      assert { result.stderr =~ /Interrupt/ }
-      result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -r cli-pasta -e sleep]
-      assert { result.stderr !~ /Interrupt/ }
-    end
+    skip NO_TIMEOUT unless TIMEOUT
+    result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -e sleep]
+    assert { result.stderr =~ /Interrupt/ }
+    result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -r cli-pasta -e sleep]
+    assert { result.stderr !~ /Interrupt/ }
   end
 
   it 'handles SIGPIPE' do
@@ -35,20 +33,16 @@ describe 'cli-pasta' do
   it 'handles just SIGINT' do
     result = sh %[#{RUBY} -r cli-pasta/sigint -e 'loop { puts "." }' | head -n0]
     assert { result.stderr =~ /EPIPE/ }
-
-    if TIMEOUT
-      result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -r cli-pasta/sigint -e sleep]
-      assert { result.stderr !~ /Interrupt/ }
-    end
+    skip NO_TIMEOUT unless TIMEOUT
+    result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -r cli-pasta/sigint -e sleep]
+    assert { result.stderr !~ /Interrupt/ }
   end
 
   it 'handles just SIGPIPE' do
     result = sh %[#{RUBY} -r cli-pasta/sigpipe -e 'loop { puts "." }' | head -n0]
     assert { result.stderr !~ /EPIPE/ }
-
-    if TIMEOUT
-      result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -r cli-pasta/sigpipe -e sleep]
-      assert { result.stderr =~ /Interrupt/ }
-    end
+    skip NO_TIMEOUT unless TIMEOUT
+    result = sh %[#{TIMEOUT} --signal INT 1 #{RUBY} -r cli-pasta/sigpipe -e sleep]
+    assert { result.stderr =~ /Interrupt/ }
   end
 end
